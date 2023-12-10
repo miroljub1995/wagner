26b68e24170f3d37e19d73f06e54a8036db232a6
Miroljub <miroljub1995@gmail.com>
2023-12-10 15:14:22 +0100

    Rename

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@
 #include "main_loop.h"
 #include "wpe_webkit.h"
 
-struct sample_state {
+struct wagner_state {
 	struct wl_display *display;
 	struct wl_listener new_output;
 	struct wl_listener new_input;
@@ -31,101 +31,100 @@ struct sample_state {
 	int dec;
 };
 
-struct sample_output {
-	struct sample_state *sample;
+struct wagner_output {
+	struct wagner_state *wagner;
 	struct wlr_output *output;
 	struct wl_listener frame;
 	struct wl_listener destroy;
 };
 
 struct sample_keyboard {
-	struct sample_state *sample;
+	struct wagner_state *wagner;
 	struct wlr_input_device *device;
 	struct wl_listener key;
 	struct wl_listener destroy;
 };
 
 static void output_frame_notify(struct wl_listener *listener, void *data) {
-	struct sample_output *sample_output =
-		wl_container_of(listener, sample_output, frame);
-	struct sample_state *sample = sample_output->sample;
-	struct wlr_output *wlr_output = sample_output->output;
+	struct wagner_output *wagner_output =
+		wl_container_of(listener, wagner_output, frame);
+	struct wagner_state *wagner = wagner_output->wagner;
+	struct wlr_output *wlr_output = wagner_output->output;
 
 	struct timespec now;
 	clock_gettime(CLOCK_MONOTONIC, &now);
 
-	long ms = (now.tv_sec - sample->last_frame.tv_sec) * 1000 +
-		(now.tv_nsec - sample->last_frame.tv_nsec) / 1000000;
-	int inc = (sample->dec + 1) % 3;
+	long ms = (now.tv_sec - wagner->last_frame.tv_sec) * 1000 +
+		(now.tv_nsec - wagner->last_frame.tv_nsec) / 1000000;
+	int inc = (wagner->dec + 1) % 3;
 
-	sample->color[inc] += ms / 2000.0f;
-	sample->color[sample->dec] -= ms / 2000.0f;
+	wagner->color[inc] += ms / 2000.0f;
+	wagner->color[wagner->dec] -= ms / 2000.0f;
 
-	if (sample->color[sample->dec] < 0.0f) {
-		sample->color[inc] = 1.0f;
-		sample->color[sample->dec] = 0.0f;
-		sample->dec = inc;
+	if (wagner->color[wagner->dec] < 0.0f) {
+		wagner->color[inc] = 1.0f;
+		wagner->color[wagner->dec] = 0.0f;
+		wagner->dec = inc;
 	}
 
 	wlr_output_attach_render(wlr_output, NULL);
 
-	struct wlr_renderer *renderer = sample->renderer;
+	struct wlr_renderer *renderer = wagner->renderer;
 	// printf("output_frame w: %d h: %d\n", wlr_output->width, wlr_output->height);
 	struct wlr_egl *egl = wlr_gles2_renderer_get_egl(wlr_output->renderer);
 	wlr_egl_make_current(egl);
 
 	wlr_renderer_begin(renderer, wlr_output->width, wlr_output->height);
 
-	glClearColor(sample->color[0], sample->color[1], sample->color[2], sample->color[3]);
+	glClearColor(wagner->color[0], wagner->color[1], wagner->color[2], wagner->color[3]);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	wlr_renderer_end(renderer);
 
 	wlr_output_commit(wlr_output);
-	sample->last_frame = now;
+	wagner->last_frame = now;
 }
 
 static void output_remove_notify(struct wl_listener *listener, void *data) {
-	struct sample_output *sample_output =
-		wl_container_of(listener, sample_output, destroy);
+	struct wagner_output *wagner_output =
+		wl_container_of(listener, wagner_output, destroy);
 	wlr_log(WLR_DEBUG, "Output removed");
-	wl_list_remove(&sample_output->frame.link);
-	wl_list_remove(&sample_output->destroy.link);
-	free(sample_output);
+	wl_list_remove(&wagner_output->frame.link);
+	wl_list_remove(&wagner_output->destroy.link);
+	free(wagner_output);
 }
 
 static void new_output_notify(struct wl_listener *listener, void *data) {
 	struct wlr_output *output = data;
-	struct sample_state *sample =
-		wl_container_of(listener, sample, new_output);
+	struct wagner_state *wagner =
+		wl_container_of(listener, wagner, new_output);
 
-	wlr_output_init_render(output, sample->allocator, sample->renderer);
+	wlr_output_init_render(output, wagner->allocator, wagner->renderer);
 
-
-	struct wlr_egl *egl_renderer = wlr_gles2_renderer_get_egl(sample->renderer);
+	struct wlr_egl *egl_renderer = wlr_gles2_renderer_get_egl(wagner->renderer);
 	assert(egl_renderer > 0);
 	wg_initialize_wpe_webkit(egl_renderer->display);
 
-	struct sample_output *sample_output =
-		calloc(1, sizeof(struct sample_output));
-	sample_output->output = output;
-	sample_output->sample = sample;
-	wl_signal_add(&output->events.frame, &sample_output->frame);
-	sample_output->frame.notify = output_frame_notify;
-	wl_signal_add(&output->events.destroy, &sample_output->destroy);
-	sample_output->destroy.notify = output_remove_notify;
+	struct wagner_output *wagner_output =
+		calloc(1, sizeof(struct wagner_output));
+	wagner_output->output = output;
+	wagner_output->wagner = wagner;
+	wl_signal_add(&output->events.frame, &wagner_output->frame);
+	wagner_output->frame.notify = output_frame_notify;
+	wl_signal_add(&output->events.destroy, &wagner_output->destroy);
+	wagner_output->destroy.notify = output_remove_notify;
 
 	struct wlr_output_mode *mode = wlr_output_preferred_mode(output);
 	if (mode != NULL) {
 		wlr_output_set_mode(output, mode);
 	}
 
-	wlr_output_commit(sample_output->output);
+	wlr_output_commit(wagner_output->output);
 }
 
 static void keyboard_key_notify(struct wl_listener *listener, void *data) {
 	struct sample_keyboard *keyboard = wl_container_of(listener, keyboard, key);
-	struct sample_state *sample = keyboard->sample;
+	struct wagner_state *wagner = keyboard->wagner;
 	struct wlr_event_keyboard_key *event = data;
 	uint32_t keycode = event->keycode + 8;
 	const xkb_keysym_t *syms;
@@ -134,7 +133,7 @@ static void keyboard_key_notify(struct wl_listener *listener, void *data) {
 	for (int i = 0; i < nsyms; i++) {
 		xkb_keysym_t sym = syms[i];
 		if (sym == XKB_KEY_Escape) {
-			wl_display_terminate(sample->display);
+			wl_display_terminate(wagner->display);
 		}
 	}
 }
@@ -149,13 +148,13 @@ static void keyboard_destroy_notify(struct wl_listener *listener, void *data) {
 
 static void new_input_notify(struct wl_listener *listener, void *data) {
 	struct wlr_input_device *device = data;
-	struct sample_state *sample = wl_container_of(listener, sample, new_input);
+	struct wagner_state *wagner = wl_container_of(listener, wagner, new_input);
 	switch (device->type) {
 	case WLR_INPUT_DEVICE_KEYBOARD:;
 		struct sample_keyboard *keyboard =
 			calloc(1, sizeof(struct sample_keyboard));
 		keyboard->device = device;
-		keyboard->sample = sample;
+		keyboard->wagner = wagner;
 		wl_signal_add(&device->events.destroy, &keyboard->destroy);
 		keyboard->destroy.notify = keyboard_destroy_notify;
 		wl_signal_add(&device->keyboard->events.key, &keyboard->key);
@@ -184,7 +183,7 @@ int main(void) {
 	wlr_log_init(WLR_DEBUG, NULL);
 	struct wl_display *display = wl_display_create();
 
-	struct sample_state state = {
+	struct wagner_state state = {
 		.color = { 1.0, 0.0, 0.0, 1.0 },
 		.dec = 0,
 		.last_frame = { 0 },
